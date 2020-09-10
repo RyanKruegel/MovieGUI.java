8000476902b38f3dcb462a10fbec2e6fba249567
Ryan Kruegel <66844168+RyanKruegel@users.noreply.github.com>
2020-09-09 21:45:12 -0400

    Add files via upload
    Initial Upload

---
+++ MovieGUI.java
@@ -0,0 +1,54 @@
+ // GUI - Based Application for Movie App
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MovieGUI extends JFrame implements ActionListener
+
+{
+	// Create the components
+	Font myFont = new Font("Verdana", Font.BOLD, 24);
+	JLabel quote = new JLabel("I'm your huckleberry...");
+	JLabel movie = new JLabel("");
+	JButton button = new JButton("Show Movie");
+
+
+	// Constructor method for this class
+	public MovieGUI()
+
+	{
+		super("Movie Quote APP");
+		setBounds(600, 340, 500, 300);
+		setLayout(new FlowLayout());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		quote.setFont(myFont);
+		movie.setFont(myFont);
+		add(quote);
+		add(button);
+		add(movie);
+		setVisible(true);
+		
+		// Add an event listener to the button component
+		button.addActionListener(this);
+	}
+
+
+	// Event handling method
+
+	@Override
+	public void actionPerformed(ActionEvent evt)
+	{
+		movie.setText("*** Tombstone (1993) ***");
+	}
+
+	// Main method to execute the application
+
+	public static void main(String[] args)
+	{
+		MovieGUI myFrame = new MovieGUI();
+	}
+
+}
